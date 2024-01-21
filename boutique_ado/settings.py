ca1ba125c69624b2c1af70500ffcafe499c723d0
Mihaly Lovrencsics <lomysa@gmail.com>
2024-01-21 18:56:31 +0000

    modify settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = os.getenv('DEBUG')
 
 
 ALLOWED_HOSTS = ['boutique-ado-mish-238fad263cca.herokuapp.com', 'localhost']
